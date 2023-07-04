526a396581057b420f890d7c37c81450a2412304
prajwalbs0606 <prajwalbs0606@gmail.com>
2023-07-04 16:47:57 +0530

    source code

---
+++ Update.java
@@ -0,0 +1,34 @@
+package jdbcDemo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Update {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		String dbUrl = "jdbc:mysql://localhost:3306/movie";
+		String username = "root";
+		String password = "Praju.s1";
+		String query = "UPDATE MOVIES SET GENERE = 'COMEDY DRAMA' where title = 'finding Nemo';";
+	
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection(dbUrl,username,password);
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate(query);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			con.close();
+		}
+
+	}
+
+}
+
